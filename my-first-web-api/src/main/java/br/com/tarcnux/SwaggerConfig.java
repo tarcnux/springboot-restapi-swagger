0ed02a7fe6efe525e4293bddd331be211d26fa3b
Tarcisio Nunes <tarcnux@gmail.com>
2022-10-18 11:04:52 -0300

    Swagger

--- my-first-web-api/src/main/java/br/com/tarcnux/SwaggerConfig.java
+++ my-first-web-api/src/main/java/br/com/tarcnux/SwaggerConfig.java
@@ -22,38 +22,8 @@ public class SwaggerConfig {
 		return new Contact("Tarcísio Nunes", "http://www.tarcnux.com.br", "tata@gmail.com");
 	}
 	
-	private ApiInfoBuilder informacoesApi() {
-
-        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-
-        apiInfoBuilder.title("Title - Rest API");
-        apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
-        apiInfoBuilder.version("1.0");
-        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
-        apiInfoBuilder.license("Licença - Sua Empresa");
-        apiInfoBuilder.licenseUrl("http://www.seusite.com.br");
-        apiInfoBuilder.contact(this.contato());
-
-        return apiInfoBuilder;
-
-    }
-    @Bean
-    public Docket detalheApi() {
-        Docket docket = new Docket(DocumentationType.SWAGGER_2);
-
-        docket
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.tarcnux.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(this.informacoesApi().build())
-                .consumes(new HashSet<String>(Arrays.asList("application/json")))
-                .produces(new HashSet<String>(Arrays.asList("application/json")));
-
-        return docket;
-    }
 	
-	/*private ApiInfoBuilder informacoesApi() {
+	private ApiInfoBuilder informacoesApi() {
 		var apiInfoBuilder = new ApiInfoBuilder();
 		
 		apiInfoBuilder.title("REST API do Tatá");
@@ -79,6 +49,6 @@ public class SwaggerConfig {
 		
 		return docket;
 	}
-	*/
+	
 
 }
